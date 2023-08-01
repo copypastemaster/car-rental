bb7f50bc200bd3339668695cd70192052a64b976
master <enoch.binas@gmail.com>
2023-08-01 23:42:06 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './containers/LandingPage'
+
+function App() {
+
+  return (
+    <> 
+      <Routes>
+        <Route path="/" element={<LandingPage />}/>
+      </Routes>
+      
+    </>
+  )
+}
+
+export default App
