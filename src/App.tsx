6f3a97e32c037ae7060d97a3f78f2b04972c11cc
master <enoch.binas@gmail.com>
2023-08-02 01:28:19 +0800

    feat: navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './containers/LandingPage'
+import Nav from './components/Nav'
 
 function App() {
 
   return (
-    <> 
+    <>
+      <Nav />
       <Routes>
         <Route path="/" element={<LandingPage />}/>
-      </Routes>
-      
+      </Routes>   
     </>
   )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import '../assets/nav.css'
+import { useState } from 'react'
+import {Avatar} from '@chakra-ui/react'
+import * as Dropdown from '@radix-ui/react-dropdown-menu'
+
+export default function Nav() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const clicked = () => {
+        setIsLoggedIn(!isLoggedIn);
+    }
+
+  return (
+    <div className='nav fixed flex justify-between items-center border rounded-full bg-white p-1 pl-5 pr-3
+                    md:mt-7 md:left-24'>
+        <h1>chf</h1>
+        <section className='flex gap-12'>
+            <p>Home</p>
+            <p>Services</p>
+            <p>Fleet</p>
+            <p>FAQ</p>
+            <p>Contact</p>
+        </section>
+        <Dropdown.Root>
+            <Dropdown.Trigger asChild>
+                <button className='butn text-white bg-cyan-500'
+                        aria-label="login"
+                        hidden={isLoggedIn}>
+                    Login
+                </button> 
+            </Dropdown.Trigger>
+            <Dropdown.Portal>
+                <Dropdown.Content className='DropdownMenuContent' sideOffset={5}>
+                    <Dropdown.Item className='item' onClick={clicked}>Login as dummy?</Dropdown.Item>
+                </Dropdown.Content>
+            </Dropdown.Portal>            
+        </Dropdown.Root>
+
+        <Dropdown.Root>
+            <Dropdown.Trigger asChild>
+                <Avatar name="User" hidden={!isLoggedIn}/>
+            </Dropdown.Trigger>
+            <Dropdown.Portal>
+                <Dropdown.Content className='DropdownMenuContent' sideOffset={5}>
+                    <Dropdown.Item className='item' onClick={clicked}>Logout</Dropdown.Item>
+                </Dropdown.Content>
+            </Dropdown.Portal>            
+        </Dropdown.Root>
+        
+    </div>
+  )
+}
+
+
