95cf844fb70afdde01795ad30c752c119a4e2a86
master <enoch.binas@gmail.com>
2023-08-09 18:07:56 +0800

    fix: service headers not aligning on smaller devices

--- src/components/ServicesHeader.tsx
+++ src/components/ServicesHeader.tsx
@@ -9,7 +9,7 @@ export default function ServicesHeader() {
     <div className='flex flex-col place-items-center md:flex-row md:flex-wrap md:justify-evenly md:gap-10 bg-black'>
         <div className='flex max-xs:gap-10 gap-10 max-xs:place-items-center sm:gap-24 md:gap-5 pt-20 md:pb-20'>
             <img src={booking} className='h-24'/>
-            <h2 className='text-white sm:max-w-[120px]  text-xl md:text-2xl'>Seamless booking</h2>
+            <h2 className='text-white max-w-[140px] sm:max-w-[120px]  text-xl md:text-2xl'>Seamless booking</h2>
         </div>
 
         <div className='flex max-xs:gap-10 gap-10 max-xs:place-items-center sm:gap-24 md:gap-5 pt-20 md:pb-20'>
@@ -24,7 +24,7 @@ export default function ServicesHeader() {
 
         <div className='flex max-xs:gap-10 gap-10 max-xs:place-items-center  sm:gap-24 md:gap-5 pt-20 pb-20'>
             <img src={money} className='h-24'/>
-            <h2 className='md:text-2xl sm:max-w-[120px]  text-xl  text-white'>No recharging fees</h2>
+            <h2 className='max-w-[140px] md:text-2xl sm:max-w-[120px]  text-xl  text-white'>No recharging fees</h2>
         </div>
     </div>
   )
